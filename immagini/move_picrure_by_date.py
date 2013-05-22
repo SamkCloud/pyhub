3080380a8ef860a2fac95ed701bb2a7fe8a5eb04
Giuliano Dedda <giuliano.dedda@yahoo.com>
2013-05-22 16:12:38 +0200

    nuova versione di move_picture_by_date.py compatibioe con python3

--- immagini/move_picrure_by_date.py
+++ immagini/move_picrure_by_date.py
@@ -1,41 +1,51 @@
 #!/usr/bin/python
 # gd 20081211
 # last modify 201305121
-# copia (o sposta) le immagini di una directory in una sottoserie di cartelle divise per data ed 2009_03_02
+# copia (o sposta) le immagini di una directory in una sottoserie di cartelle divise per data es 2009_03_02
+# utilizza imagemagic
 
-
-
-
-
-
-def getExifDate(fileName):
-		sys.path.append("~/bin/immagini")
-		import EXIF
-		file = open(fileName,"rb")
-		data = EXIF.process_file(file)
-		if not data:
-			return "No EXIF Information available."
-		
-		timestamp = data['EXIF DateTimeDigitized']
-		print(timestamp)
-		data = str(timestamp)
-		out = data[0:10].replace(':','_')
-		return out
-		
 import os,sys
 import shutil
 from os.path import join, getsize
+import subprocess
 
+def execute_command(comando):
+	"""
+	Esegue un comando di sistema
+	"""
+	verbose=False
+	simulate=False
+	if verbose == True:
+		print ('--------------------------------------')
+		print (comando)
+		print ('++++++++++++++++++++++++++++++++++++++')
+	if simulate == False:
+		
+		status = 0
+#		comando = "identify"
+		
+
+		out = subprocess.check_output(comando,shell=True)
+		return out
+
+
+def getExifDate(fileName):
+		comando = 'identify -format "%[EXIF:DateTime]" "' + fileName+'"'
+		output = execute_command(comando)
+		out2=str(output)
+	
+		out = out2[2:12].replace(':','_')
+		return out
+		
 directory_base = sys.argv[1]
-print (directory_base)
 
 for root, dirs, files in os.walk(directory_base):
 	for name in files:
 		file = os.path.join(root, name)
-		dir = directory_base+'/'+getExifDate(file)
-		if not os.path.exists(dir):
-			os.mkdir(dir)
-		print(file+'  '+dir+'/'+name)
-		shutil.copyfile(file,dir+'/'+name)			
-		#shutil.move(file,dir+'/'+name)
+		dir = directory_base+'/new/'+getExifDate(file)
+#		if not os.path.exists(dir):
+#			os.mkdir(dir)
+		print(file+'  '+dir+'_'+name)
+		shutil.copyfile(file,dir+'_'+name)			
+		#shutil.move(file,dir+'_'+name)
 	
