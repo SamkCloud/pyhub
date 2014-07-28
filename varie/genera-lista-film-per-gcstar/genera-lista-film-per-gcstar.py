9fcbc2fd6469dbe0eff5c8dec72cde799883768c
Giuliano Dedda <giuliano.dedda@yahoo.com>
2014-07-28 15:45:12 +0200

    piccole modifiche arc27

--- varie/genera-lista-film-per-gcstar/genera-lista-film-per-gcstar.py
+++ varie/genera-lista-film-per-gcstar/genera-lista-film-per-gcstar.py
@@ -7,16 +7,20 @@ import argparse
 import subprocess
 import shlex
 import re
+import time
 
 def print_r(v):
 	return '%s = %r %s' % (v, v, type(v))
 
-def show_file_in_dir(dir):
+def show_file_in_dir(dir,args):
+	f = open(args.output,'a')
 	for dirpath, dirnames, filenames in os.walk (dir):
 		for subdir in dirnames:
-			show_file_in_dir(subdir)
+			show_file_in_dir(subdir,args)
 		for nomefile in filenames:
 			nome_file_completo = os.path.join(dirpath,nomefile)
+			(mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(nome_file_completo)
+
 			#print (nomefile)
 			parts = nomefile.split("-") 
 		
@@ -33,23 +37,32 @@ def show_file_in_dir(dir):
 			output = p.communicate()
 			out_list = (str( output[0], encoding='utf8' )).split("\n")
 			commento = ''
+			lista_commenti = []
 			for line in out_list:
 				if re.search('Stream #', line,re.I):
-					commento += line+"|"
+					lista_commenti.append(line.strip())
 
-			
-			
-			print (''+titolo+';'+nome_file_completo+';'+commento+'')
+			if (mtime > int(args.newer_time)):
+				commento = '|'.join(lista_commenti)
+				f.write(''+titolo+';'+nome_file_completo+';'+commento+'\n') 
+	f.close()		
 
 def run(args):
-	print ("Titolo;Trailer;Commento")
-	show_file_in_dir(args.dirname)	 
+	
+	f = open(args.output,'w')
+	f.write("Titolo;Trailer;Commento\n") 
+	f.close()
+	#print("Titolo;Trailer;Commento") 
+	show_file_in_dir(args.dirname,args)	 
+
 	
 	 
 def main():
 
 	parser = argparse.ArgumentParser(description='Genera la lista film in csv da importare in gcstar')
 	parser.add_argument("dirname", help='directory in cui ci sono i film da analizzare')
+	parser.add_argument('-o','--output',help='Output file name', default='lista-film.csv')
+	parser.add_argument('-t','--newer-time',help='Only file newer than (Unix Time)', default=0)
 	args = parser.parse_args()
 	
 	run(args)
