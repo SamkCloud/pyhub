d739841ff9b8890885d868862af8541733215305
Giuliano Dedda <giuliano.dedda@yahoo.com>
2015-02-16 07:12:19 +0100

    piccole modifiche

--- video/convert2mkv.py
+++ video/convert2mkv.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 # gd 20150215
 """
-Convert file to 
+Convert file to matroska mkv
 """
-
 
 import argparse
 import sys
@@ -16,7 +15,7 @@ def print_r(v):
 def run(arguments):
 	print ("Input file: %s" % arguments.input )
 
-	if arguments.codifica == 'h264old':
+	if arguments.vcodec == 'h264old':
 		# le opzioni prese da: http://ubuntuforums.org/showthread.php?t=786095
 		# il parametro -crf imposta la qualità  (più basso = migliore( valori da 18 a 28)
 		# con 22 la qualità  è molto buona
@@ -29,34 +28,33 @@ def run(arguments):
 		# NB per i filmati .MOV della Panasonic TZ5 usare bitrate 32kb
 		
 		#video_options = "-vcodec libx264 -preset veryslow -crf 20 -threads 0"
-		# Per Panasonic TZ5
-		#audio_opt = "-acodec aac -ar 8000 -ab 32k -ac 1 -strict -2"  
-		# per Olimpus XZ-1
-		#audio_opt = "-acodec aac -ab 128k -ac 2 -strict -2"
-		
-		
 		video_options 	= "-vcodec libx264 -preset veryslow -crf 20 -threads 0"
-		audio_opt 		= "-acodec copy"
-
-	elif arguments.codifica == 'h264':
+	elif arguments.vcodec == 'h264':
 		video_options 	= "-c:v libx264"
-		audio_opt 		= "-c:a copy"
-		
-		
-	elif arguments.codifica == 'h265':
+	elif arguments.vcodec == 'h265':
 		video_options 	= "-c:v libx265"
+	elif arguments.vcodec == 'loseless':
+		video_options = "-c:v libx264 -preset veryslow -qp 0"
+	else:
+		sys.exit("Error: Codifica Video non riconosciuta")
+	
+	if arguments.acodec == 'copy':
 		audio_opt 		= "-c:a copy"
+	elif arguments.acodec == 'aac_panasonic':
+	#	Per Panasonic TZ5
+		# probabilmente non serve usarla, il bitrate è basso e la perdita di qualità alta
+		print("aac_panasonic peggiora la qualità di e la traccia audio è piccola)
+		audio_opt = "-acodec aac -ar 8000 -ab 32k -ac 1 -strict -2"  
+	elif arguments.acodec == 'aac':
+	#	per Olimpus XZ-1
+		audio_opt = "-acodec aac -ab 128k -ac 2 -strict -2"
+	else:
+		sys.exit("Error: Codifica Audio non riconosciuta")
 		
 	
-	elif arguments.codifica == 'loseless':
-		video_options = "-c:v libx264 -preset veryslow -qp 0"
-		audio_opt = "-acodec aac -ab 128k -ac 2 -strict -2"
-
 	
-	else:
-		sys.exit("Error: Codifica non riconosciuta")
 	
-	output_filename = arguments.input+'.'+arguments.codifica+'.mkv'
+	output_filename = arguments.input+'.'+arguments.vcodec+'.mkv'
 	log_filename =arguments.input+'.log'
 	comando = 'ffmpeg -y -i "' + arguments.input + '" ' + video_options + ' ' + audio_opt + ' "' + output_filename + '" 2> "' + log_filename + '"'
 	print(comando)
@@ -74,7 +72,8 @@ def main():
 
 	parser = argparse.ArgumentParser(description='Demo of argparse')
 	parser.add_argument('input')
-	parser.add_argument('--codifica','-c',default='h264',help='Scegli il tipo di codifica (h264|loseless|h264old|h265)')
+	parser.add_argument('--vcodec','-c',default='h264',help='Scegli il tipo di codifica (h264|loseless|h264old|h265)')
+	parser.add_argument('--acodec','-a',default='copy',help='Scegli il tipo di codifica (copy|aac|aac_panasonic)')
 	
 	arguments = parser.parse_args()
 
